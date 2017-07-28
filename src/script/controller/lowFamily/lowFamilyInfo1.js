7d939775b885677c2e6835da05e8ada53ab47e53
黑猫 <maweichao90@icloud.com>
2017-07-28 14:25:19 +0800

    a

---
+++ src/script/controller/lowFamily/lowFamilyInfo1.js
@@ -0,0 +1,12 @@
+var familyInfo = angular.module("familyInfo", ['ngFileUpload']);
+
+familyInfo.controller("familyInfoCtro", ["$scope", "$filter", "$http", function($scope, $filter, $http) {
+	var familyInfo = {} || familyInfo;
+	//获取参数
+	//	familyInfo.urlParam = $stateParams;
+
+	familyInfo.sendParam = {};
+	familyInfo.list = {};
+	$scope.familyInfo = familyInfo;
+
+}]);
--- src/script/controller/lowFamily/lowFamilyList.js
+++ src/script/controller/lowFamily/lowFamilyList.js
@@ -1,48 +1,16 @@
-myApp.controller("low_family_List", ["$scope", "$state", "$filter", "$http", "$stateParams", function($scope, $state, $filter, $http, $stateParams) {
-	var low_family_List = {} || low_family_List;
+//经纪人列表
+myApp.controller("lowFamilyListCtro", ["$scope", "$state", "$filter", "$http", "$stateParams",function($scope, $state, $filter, $http, $stateParams) {
+	var lowFamilyList = {} || lowFamilyList;
 	//获取参数
-	low_family_List.urlParam = $stateParams;
-	low_family_List.sendParam = {};
-	low_family_List.page = {
+	lowFamilyList.urlParam = $stateParams;
+	lowFamilyList.sendParam = {};
+	lowFamilyList.page = {
 		page: 1,
-		size: 10
+		size: 15,
 	};
-	
-	low_family_List.list = {};
-
+	lowFamilyList.list = {};
 	//获取当前用户信息  
-	low_family_List.redList = function(args) {
-		low_family_List.page.page = args ? args : 1;
-		var param = angular.extend(low_family_List.sendParam, low_family_List.page);
-		$http.get(Config.rqUrl.low_family_List, {
-			params: param,
-		}).success(function(data) {
-			if(data.status) {
-				var oprationObj = data.data.data;
-				low_family_List.listNum = data.data.count;
-					
-				//映射认证状态
-				$.each(oprationObj, function(n, m) {
-					angular.extend(m, {
-						headUrlAll: imgThumbnailUrl + m.headUrl
-					})
-					angular.extend(m, {
-						userStatusObj: $.stateMap(Config.stateMap.userStatus, m.userStatus)
-					})
-				});
 
-				low_family_List.list = oprationObj;
-				getPage.getPage(Math.ceil(data.data.count / low_family_List.page.size), low_family_List.page.page, "doctor_page", function(num, type) {
-					low_family_List.redList(num);
-				});
-			} else {
-				$.alertError("获取经纪人信息错误," + data.err)
-			}
-		})
-	}
-
-	//调用列表
-	low_family_List.redList();
 	//根据角色遍历响应的菜单
-	$scope.low_family_List = low_family_List;
+	$scope.lowFamilyList = lowFamilyList;
 }]);
