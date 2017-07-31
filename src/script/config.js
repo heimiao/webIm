76e8e7dd1030c57c82fcb44610a3c29670ec3ea3
liuxinhui <liuxinhui@datangnet.com.cn>
2017-07-31 19:48:54 +0800

    123

--- src/script/config.js
+++ src/script/config.js
@@ -3,14 +3,13 @@ var config = {
 	//请求路径
 	path: {
 		login: baseUrl + "/dotpalogin",
-<<<<<<< HEAD
+
 		townShip: baseUrl+ "/zcjg/queryForZc",  //获取乡镇列表
 		townShip: baseUrl+ "/zcjg/queryForZc",  //获取乡镇列表
 		naturalVillage: baseUrl+ "/zrc/queryForPage",  //自然村列表
-		//naturalbianji: baseUrl+ "/zrc/naturalEdite",  //自然村详情编辑
-=======
+		
 		townShip: baseUrl+ "/zcjg/queryForZc?lx=01",  //获取乡镇列表
->>>>>>> 5e45e322d1008ac767c7e878fa979c5d963718fa
+
 	},
 	//系统基本信息配置
 	sysInfo: {
