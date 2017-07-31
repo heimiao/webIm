b6b77d635f7d15fbba9c564e18f20b5555265303
hexiangshuai <hexiangshuai@datangnet.com.cn>
2017-07-31 20:24:25 +0800

    aa

--- src/script/config.js
+++ src/script/config.js
@@ -4,6 +4,8 @@ var config = {
 	path: {
 		login: baseUrl + "/dotpalogin",
 		townShip: baseUrl+ "/zcjg/queryForZc?lx=01",  //获取乡镇列表
+		villageAll: baseUrl + "/zcjg/queryForZc?lx=02&fid=", //获取所有行政村列表
+		villageList: baseUrl + "/pkc/queryForPage"
 	},
 	//系统基本信息配置
 	sysInfo: {
--- src/script/controller/village/village.js
+++ src/script/controller/village/village.js
@@ -1,18 +1,75 @@
-myApp.controller("poorVillage", ["$scope", "$state", "$http", "$stateParams",
-	function($scope, $state, $http, $stateParams) {
+myApp.controller("poorVillage", ["$scope", "$state", "$http", "$stateParams","postForm",
+	function($scope, $state, $http, $stateParams, postForm) {
 		var poorVillage = {} || poorVillage;
 		poorVillage.urlParam = $stateParams;
 		poorVillage.sendParam = {};
-		// var data={
-		// 	"lx": 01
-		// }
-		console.log("123333"+JSON.stringify(config));
+		poorVillage.list = {}; //查询列表
+		poorVillage.townShip = []; //全部乡镇列表
+		poorVillage.villageList = []; //全部行政村列表
+		poorVillage.sendParam.townShipId = null;
+		poorVillage.sendParam.villageListId = null;
+		poorVillage.sendParam.year = null;
+		poorVillage.sendParam.time = null;
 		$http.post(config.path.townShip,null).success(function(res){
-			console.log(1)
-			console.log(res)
-
+			poorVillage.townShip = res;
 		})
+		poorVillage.chooseTownship=function(name,id){
+			poorVillage.sendParam.townShipId = id;
+			$("#township .name").html(name)
+			$(".townshipList").slideUp(200)
+			$("#township").removeClass("township2")
+			$("#township .name").removeClass("col-ea3c4c")
+			$http.post(config.path.villageAll+id,null).success(function(res){
+				poorVillage.villageList = res;
+				console.log(poorVillage.villageList)
+			})
+		}
+		poorVillage.chooseVillage=function(name,id){
+			poorVillage.sendParam.villageListId = id;
+			$("#village .name").html(name)
+			$(".villageList").slideUp(200)
+			$("#village").removeClass("township2")
+			$("#village .name").removeClass("col-ea3c4c")
+		}
+		poorVillage.chooseTime=function(time){
+			poorVillage.sendParam.time = time;
+			$("#time .name").html(time)
+			$(".timeList").slideUp(200)
+			$("#time").removeClass("township2")
+			$("#time .name").removeClass("col-ea3c4c")
+		}
 
+		// $http.post(config.path.villageList+'nd='+poorVillage.sendParam.time+'&limit=10&start=0&qyxz='+poorVillage.sendParam.townShipId+'&qyxzc='+poorVillage.sendParam.villageListId,null).success(function(res){
+		// 	poorVillage.townShip = res;
+		// })
+		poorVillage.datas = {
+			"nd": poorVillage.sendParam.time,
+			"limit":　10,
+			"start": 0,
+			"qyxz": poorVillage.sendParam.townShipId,
+			"qyxzc": poorVillage.sendParam.villageListId,
+		}
+		postForm.saveFrm(config.path.villageList,poorVillage.datas).success(function(res){
+			if(res.success){
+				for(var r=0;r<res.results.length;r++){
+					for(var i=0;i<poorVillage.townShip.length;i++){
+						if(res.results[r].qyxz == poorVillage.townShip[i].id){
+							res.results[r].qyxz = poorVillage.townShip[i].name
+						}
+					}
+				}
+				for(var r=0;r<res.results.length;r++){
+					for(var i=0;i<poorVillage.villageList.length;i++){
+						if(res.results[r].qyxzc == poorVillage.villageList[i].id){
+							res.results[r].qyxzc = poorVillage.villageList[i].name
+						}
+					}
+				}
+				poorVillage.list = res.results;
+			}
+		})
+		 // poorVillage.list=data.poorVillage.results;
+		
 		/*poorVillage.menu=false;
 		poorVillage.changeMenu=function(args){
 			poorVillage.menu=args;
