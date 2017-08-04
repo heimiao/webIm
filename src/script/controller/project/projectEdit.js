d17a089b81fe7671caaf158f9ac45ef14eae86f7
liuxinhui <liuxinhui@datangnet.com.cn>
2017-08-04 18:54:03 +0800

    s

--- src/script/controller/project/projectEdit.js
+++ src/script/controller/project/projectEdit.js
@@ -3,16 +3,39 @@ myApp.controller("projectEdit", ["$scope", "$state", "$http", "$stateParams",
 		var projectEdit = {} || projectEdit;
 		projectEdit.urlParam = $stateParams;
 		projectEdit.sendParam = {};
+		projectEdit.townShip = []; //全部乡镇列表
+		projectEdit.villageListAll = []; //获取全部行政村
+		// 获取所有乡镇
+		$http.post(config.path.townShip,null).success(function(res){
+			projectEdit.townShip = res;
+			projectEdit.sendParam.qyxz=res[0].id;
+			projectEdit.getVillageList(res[0].id, 1); //获取乡镇对应的行政村
+		})
+		// 获取所有行政村
+		projectEdit.getVillageList= function(id, num){
+			$http.post(config.path.villageAll+"&fid="+id,null).success(function(res){
+				projectEdit.sendParam.qyxzc = res[0].id;
+				projectEdit.villageListAll = res;
+			})
+		}
 
+		// 乡镇变化行政村跟随变化
+		projectEdit.changeTown=function(){
+			projectEdit.getVillageList(projectEdit.sendParam.qyxz, 1); //获取乡镇对应的行政村
+		}
 
-		console.log(projectEdit.urlParam);
+		//从数据字典获取项目类型的选项
+		projectEdit.getpeojectlx= function(){
+			$http.post(config.path.projectsjzd,null).success(function(res){
+				projectEdit.xmleList=res;
+				projectEdit.sendParam.xmlx=projectEdit.xmleList[0].id; //默认选中第一个
+			})
+		}
+		projectEdit.getpeojectlx()
 
-		/*lowFamilyInfo.menu=false;
-		lowFamilyInfo.changeMenu=function(args){
-			lowFamilyInfo.menu=args;
-			console.log(lowFamilyInfo.menu);
-		}*/
+		
 
+		
 		//调用列表
 		//		$state.go('lowFamily.baseInfo'); //默认显示第一个tab
 		//根据角色遍历响应的菜单
